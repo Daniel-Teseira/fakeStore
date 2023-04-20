5b6b8b24240948789c7c9f43a0d87b2aaaa2d994
Daniel Teseira <daniel.teseira@alu.frt.utn.edu.ar>
2023-04-19 23:37:41 -0300

    se adapto estuilo mobile

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Nav from './components/Navbar/Navbar';
 import Cart from './pages/Cart/Cart';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
-import ProductDetail from './pages/ProductDetail/ProductDetail';
+import Producto from './pages/Producto/Producto';
 // import Register from './pages/Register/Register';
 // import Registerv2 from './pages/Register/Registerv2';
 import Registerv3 from './pages/Register/Registerv3';
@@ -18,8 +18,7 @@ function App() {
 
         <Route exact path='/' element={ <Home /> } />
         
-        <Route path='/product-detail/:id' element={ <ProductDetail /> } />
-        {/* TODO: Crear rutas de login y registro */}
+        <Route path='/producto/:id' element={ <Producto /> } />
         <Route path='/login' element={ <Login /> } />
         <Route path='/register' element={ <Registerv3 /> } />
         <Route path='/cart' element={<Cart />} />
