83ff51f1441d17bf0cd6fb693fd123fc7b03f2a2
Daniel Teseira <daniel.teseira@alu.frt.utn.edu.ar>
2023-04-20 23:26:45 -0300

    se agrego contador de cantidad de producto y actualizacion del precio con la cantidad

--- src/pages/Producto/Producto.js
+++ src/pages/Producto/Producto.js
@@ -6,6 +6,9 @@ const Producto = () => {
   const params = useParams()
   const [product, setProduct] = useState(null)
 
+  const [cantidad, setCantidad] = useState('1')
+
+
   const fetchProduct = async (id) => {
     const response = await fetch(`https://fakestoreapi.com/products/${id}`)
     const data = await response.json()
@@ -17,27 +20,39 @@ const Producto = () => {
     fetchProduct(params.id)
   }, [params.id])
 
-  if (!product) return <h3 className='text-center mt-3'>Cargando producto...</h3>
+  if (!product) return <h3 className='text-center mt-3 text-white'>Cargando producto...</h3>
 
   return (
     <section className='product-detail-container container p-4 mt-5'>
       <p><h3 className='d-flex justify-content-center'> {product.title} </h3></p>
-
+      
       <div className='d-flex row contenido'>
         <img src={product.image} className='col-4 image' alt={product.title}/>
 
         <div className='d-flex flex-column col-8 info'>
           <p> {product.description} </p>
           {/* <span class="badge bg-secondary col-2"> {product.category} </span> */}
-          <h4 className='mt-3'>${product.price}</h4>
-                  
+          {/* <h4 className='mt-3'>${product.price}</h4> */}
+          <h4 className='mt-3'>${ cantidad==0 ? product.price*1 : product.price*parseInt(cantidad)}</h4>
+
+          {/* {console.log(cantidad)} */}
+          {/* {console.log(product.price)}
+          {console.log("Hola")}
+          {console.log(product.price*parseInt(cantidad))} */}
+          {/* {console.log(event.target.value)} */}
+         
+
           <div className='d-flex align-items-center mx-5  justify-content-end comprar'>
             {/* <p className='mx-3 my-0'> Hay {product.rating.count} 
                 disponible!! 
             </p> */}
              <h5 className='my-0'> Cantidad 
             </h5>
-            <select className="form-select mx-3" aria-label="Default select example">              
+            <select 
+              className="form-select mx-3" 
+              aria-label="Default select example" 
+              onChange={(event) => setCantidad(event.target.value)}
+              >                         
               <option value="1" selected>1</option>
               <option value="2">2</option>
               <option value="3">3</option>
